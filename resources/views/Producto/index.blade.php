297baeaa37ec18f35b8acae115291b66fca34be1
Marcos Alberto Saavedra <once.marcos.alberto@gmail.com>
2019-06-16 11:06:05 -0500

    actualizacion 6.16

--- resources/views/Producto/index.blade.php
+++ resources/views/Producto/index.blade.php
@@ -30,6 +30,151 @@
 
 
 <div class=" row ">
+
+
+
+<!--
+<div class="col-lg-12  ">
+
+<div class="card-body">  
+    <div id="example_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6">
+
+
+
+
+
+
+
+                        </div>
+                     
+		                    </div><div class="row"><div class="col-sm-12">
+                            	<table id="myTables" style="width: 100%;"  class="table table-hover table-striped table-bordered dataTable dtr-inline" role="grid" aria-describedby="example_info">
+                                <thead>
+                                <tr role="row">
+
+                                	<th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 254.2px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
+
+                                	<th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 254.2px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
+                                	<th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 254.2px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
+                                	<th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 254.2px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
+                                	<th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 254.2px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
+                                	<th class="sorting_asc" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 254.2px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
+
+
+
+                                	<th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 388.2px;" aria-label="Position: activate to sort column ascending">Position</th>
+                                	<th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 188.2px;" aria-label="Office: activate to sort column ascending">Office</th>
+                                	<th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 108.2px;" aria-label="Age: activate to sort column ascending">Age</th>
+                                	<th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 195.2px;" aria-label="Start date: activate to sort column ascending">Start date</th>
+                                	<th class="sorting" tabindex="0" aria-controls="example" rowspan="1" colspan="1" style="width: 151.2px;" aria-label="Salary: activate to sort column ascending">Salary</th></tr>
+                                </thead>
+                                <tbody>
+                                
+                                
+                                <tr role="row" class="odd">
+                                    <td tabindex="0" class="sorting_1">Airi Satou</td>
+                                    <td>Accountant</td>
+                                    <td>Tokyo</td>
+                                    <td>33</td>
+                                    <td>2008/11/28</td>
+                                    <td>$162,700</td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td class="sorting_1" tabindex="0">Angelica Ramos</td>
+                                    <td>Chief Executive Officer (CEO)</td>
+                                    <td>London</td>
+                                    <td>47</td>
+                                    <td>2009/10/09</td>
+                                    <td>$1,200,000</td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td tabindex="0" class="sorting_1">Ashton Cox</td>
+                                    <td>Junior Technical Author</td>
+                                    <td>San Francisco</td>
+                                    <td>66</td>
+                                    <td>2009/01/12</td>
+                                    <td>$86,000</td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td class="sorting_1" tabindex="0">Bradley Greer</td>
+                                    <td>Software Engineer</td>
+                                    <td>London</td>
+                                    <td>41</td>
+                                    <td>2012/10/13</td>
+                                    <td>$132,000</td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td class="sorting_1" tabindex="0">Brenden Wagner</td>
+                                    <td>Software Engineer</td>
+                                    <td>San Francisco</td>
+                                    <td>28</td>
+                                    <td>2011/06/07</td>
+                                    <td>$206,850</td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td tabindex="0" class="sorting_1">Brielle Williamson</td>
+                                    <td>Integration Specialist</td>
+                                    <td>New York</td>
+                                    <td>61</td>
+                                    <td>2012/12/02</td>
+                                    <td>$372,000</td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td class="sorting_1" tabindex="0">Bruno Nash</td>
+                                    <td>Software Engineer</td>
+                                    <td>London</td>
+                                    <td>38</td>
+                                    <td>2011/05/03</td>
+                                    <td>$163,500</td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td class="sorting_1" tabindex="0">Caesar Vance</td>
+                                    <td>Pre-Sales Support</td>
+                                    <td>New York</td>
+                                    <td>21</td>
+                                    <td>2011/12/12</td>
+                                    <td>$106,450</td>
+                                </tr>
+                                <tr role="row" class="odd">
+                                    <td class="sorting_1" tabindex="0">Cara Stevens</td>
+                                    <td>Sales Assistant</td>
+                                    <td>New York</td>
+                                    <td>46</td>
+                                    <td>2011/12/06</td>
+                                    <td>$145,600</td>
+                                </tr>
+                                <tr role="row" class="even">
+                                    <td tabindex="0" class="sorting_1">Cedric Kelly</td>
+                                    <td>Senior Javascript Developer</td>
+                                    <td>Edinburgh</td>
+                                    <td>22</td>
+                                    <td>2012/03/29</td>
+                                    <td>$433,060</td>
+                                </tr></tbody>
+                                <tfoot>
+                                
+                                <tr><th rowspan="1" colspan="1">Name</th><th rowspan="1" colspan="1">Position</th><th rowspan="1" colspan="1">Office</th><th rowspan="1" colspan="1">Age</th><th rowspan="1" colspan="1">Start date</th><th rowspan="1" colspan="1">Salary</th></tr>
+                                </tfoot>
+                            </table>
+
+                        </div></div>
+
+                       </div>
+                        </div>
+                        
+
+                        </div>
+
+
+
+
+
+
+-->
+
+
+
+
 	<div class="col-lg-12  ">
 		<div class="main-card mb-3 card">
 			<div class="card-body">
@@ -40,19 +185,22 @@
 				@endcan
 				<div class=""> // add this div
 
-					<table id="myTable" class="table table-hover display" cellspacing="0" width="100%">
+					<table id="myTable" style="width: 100%;"  class="table table-hover table-striped table-bordered dataTable dtr-inline" role="grid" aria-describedby="example_info">
 						<thead>
 							<tr>
 								<th>id</th>
 								<th>nombre_proveedor</th>
 								<th>nombre</th>
 								<th>imagen</th>
+								<!--
 								<th>precio_caja</th>
 								<th>cantidad_caja</th>
 								<th>precio_unidad</th>
 								<th>iva</th>
 								<th>porcentaje_ganacia</th>
+							-->
 								<th>precio_venta</th>
+								<
 								<th>ganacia</th>
 								<th>proveedor_id</th>
 								<th>Ultima Modificacion</th>
@@ -73,11 +221,13 @@
 										</img>
 									</a>
 								</td>
+								<!--
 								<td>{{ $lists->precio_caja }}</td>
 								<td>{{ $lists->cantidad_caja }}</td>
 								<td>{{ $lists->precio_unidad }}</td>
 								<td>{{ $lists->iva }}</td>
 								<td>{{ $lists->porcentaje_ganacia }}</td>
+								-->
 								<td>{{ $lists->precio_venta }}</td>
 								<td>{{ $lists->ganacia }}</td>
 								<td>
@@ -149,7 +299,7 @@
 		</div>
 	</div>
 </div>
-
+-->
 
 
 
